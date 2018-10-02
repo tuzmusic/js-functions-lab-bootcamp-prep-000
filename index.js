0f4aa19ec752370e83702f857497707623a76bf0
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-02 23:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `It's ${days} until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
